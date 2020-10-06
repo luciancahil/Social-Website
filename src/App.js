1c74524373b88f2b61c7c0cc559e215ac729584c
Roy He <royhe62@yahoo.ca>
2020-10-05 21:21:57 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './App.css';
 import Userpage from './Components/Userpage.component';
 import NavBar from './Components/Navbar.component';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 
 function App() {
   return (
-    <div className="App">
-        <NavBar/>
-        <Userpage/>
-    </div>
+    <Router>
+        <Route path = "/" exact compontent = {<NavBar/>}/>
+        <Route path = "/user" exact compontent = {<Userpage/>}/>
+
+    </Router>
   );
 }
+// /            <Route path="/" exact render={(props) => <HomePage {...props} username = {this.state.username} chageUser = {this.changeUser} randomSession = {this.state.randomSession} quickStart = {this.quickStart}/>} />
 
 export default App;
